d62651f55ccaf218c1d585eee9c69492b79ce200
Ramya1520 <ramya.krishnan1520@gmail.com>
2023-05-16 08:50:47 +0530

    login form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import { useState } from "react";
+import './App.css'
+const list=()=>{
+return(
+  <div className="login-form">
+<div className="form">
+  <div className="col-lg-4 form_border">
+  <h3>Login form</h3>
+  <div>
+  <input placeholder="Username,Useremail,Phonenumber" />
+  </div>
+  <div>
+  <input placeholder="Password"/>
+  </div>
+  <div>
+  <button type="Submit" className="btn btn-primary">Submit</button>
+  </div>
+  <div className="forget_create">
+  <p>Forget Password? </p>
+  <p className="or">or </p>
+  <p>Create Account</p>
+  </div>
+  </div>
+</div>
+  </div>
+)
 }
-
-export default App;
+export default list
